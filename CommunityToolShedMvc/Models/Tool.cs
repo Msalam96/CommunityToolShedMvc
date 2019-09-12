566cb0705bcc329f81dfcfe18862ea8073f79450
Mohammed Salam <mohammed.salam912@myhunter.cuny.edu>
2019-09-12 16:21:36 -0400

    Implemented adding community adding

---
+++ CommunityToolShedMvc/Models/Tool.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityToolShedMvc.Models
+{
+    public class Tool
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public int OwnerId { get; set; }
+        public string Usage { get; set; }
+        public string Warning { get; set; }
+        public string Age { get; set; }
+        public string OwnerName { get; set; }
+    }
+}
