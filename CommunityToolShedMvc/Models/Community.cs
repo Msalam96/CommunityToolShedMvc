7c253ab7ee891d39bd85b3192973d50cf6e6728b
Mohammed Salam <mohammed.salam912@myhunter.cuny.edu>
2019-09-12 11:55:49 -0400

    List communities user belongs to on Home Page

---
+++ CommunityToolShedMvc/Models/Community.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityToolShedMvc.Models
+{
+    public class Community
+    {
+        public int Id { get; set; }
+        public string CommunityName { get; set; }
+        public bool Open { get; set; }
+        public int OwnerId { get; set; }
+        public string OwnerName { get; set; }
+    }
+}
