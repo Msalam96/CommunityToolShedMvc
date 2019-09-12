e2d199564cd241345bc84f8bd5da74d3cc93f80f
Mohammed Salam <mohammed.salam912@myhunter.cuny.edu>
2019-09-12 09:46:18 -0400

    Initial Project with registration and login

---
+++ CommunityToolShedMvc/Controllers/AccountController.cs
@@ -0,0 +1,98 @@
+﻿using CommunityToolShedMvc.Data;
+using CommunityToolShedMvc.Models;
+using CommunityToolShedMvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CommunityToolShedMvc.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            var viewModel = new RegisterViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Register(RegisterViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                string hashedPassword = BCrypt.Net.BCrypt.HashPassword(viewModel.Password, 12);
+
+                DatabaseHelper.Insert(@"
+                    insert Person (
+                        FirstName, 
+                        LastName, 
+                        Email, 
+                        HashedPassword
+                    ) values (
+                        @FirstName, 
+                        @LastName, 
+                        @Email, 
+                        @HashedPassword
+                    )
+                ",
+                    new SqlParameter("FirstName", viewModel.FirstName),
+                    new SqlParameter("LastName", viewModel.LastName),
+                    new SqlParameter("@Email", viewModel.Email),
+                    new SqlParameter("@HashedPassword", hashedPassword));
+
+                FormsAuthentication.SetAuthCookie(viewModel.Email, false);
+                return RedirectToAction("Index", "Home");
+            }
+            return View(viewModel);
+        }
+        // GET: Account
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            var ViewModel = new LoginViewModel();
+            return View(ViewModel);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel viewModel)
+        {
+
+            if(ModelState.IsValidField("Email") && ModelState.IsValidField("Password"))
+            {
+                Person person = DatabaseHelper.RetrieveSingle<Person>(@"
+                    select HashedPassword
+                    from Person
+                    where Email = @Email
+                ",
+                    new SqlParameter("@Email", viewModel.Email));
+
+                if (person == null || !BCrypt.Net.BCrypt.Verify(viewModel.Password, person.HashedPassword))
+                {
+                    ModelState.AddModelError("", "Login Failed.");   
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                FormsAuthentication.SetAuthCookie(viewModel.Email, false);
+                return RedirectToAction("Index", "Home");
+            }
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ CommunityToolShedMvc/Global.asax.cs
@@ -0,0 +1,62 @@
+﻿using CommunityToolShedMvc.Data;
+using CommunityToolShedMvc.Models;
+using CommunityToolShedMvc.Security;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace CommunityToolShedMvc
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_PostAuthenticateRequest()
+        {
+            IPrincipal user = HttpContext.Current.User;
+
+            if (user.Identity.IsAuthenticated && user.Identity.AuthenticationType == "Forms")
+            {
+                FormsIdentity formsIdentity = (FormsIdentity)user.Identity;
+                FormsAuthenticationTicket ticket = formsIdentity.Ticket;
+
+                CustomIdentity customIdentity = new CustomIdentity(ticket);
+
+                string currentUserEmail = ticket.Name;
+                Person person = DatabaseHelper.RetrieveSingle<Person>(@"
+                    select Id, FirstName, LastName, Email
+                    from Person 
+                    where Email = @Email
+                ",
+                    new SqlParameter("@Email", currentUserEmail));
+
+                person.Roles = DatabaseHelper.Retrieve<CommunityRole>(@"
+                    select r.RoleName, c.Id
+                    from Person P
+                    join PersonRole pr on pr.PersonId = p.id
+                    join Role r on pr.RoleId = r.id
+                    join Community c on c.Id = pr.CommunityId
+                    where pr.PersonId = @Id
+                ",
+                    new SqlParameter("Id", person.Id));
+
+                CustomPrincipal customPrinicipal = new CustomPrincipal(customIdentity, person);
+
+                HttpContext.Current.User = customPrinicipal;
+                Thread.CurrentPrincipal = customPrinicipal;
+            }
+        }
+
+    }
+}
